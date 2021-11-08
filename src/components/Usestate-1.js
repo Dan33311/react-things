9dbb683efe1898ef4a3ec61946bb2105d40e9f04
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-11-07 23:30:04 -0500

    -

--- src/components/Usestate-1.js
+++ src/components/Usestate-1.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
+
 const Counter = () => {
 
   const [count, setCount] = useState(0)
+  const [clicked, setClicked] = useState(0)
 
   const increment = () => {
     setCount( count + 1 )
@@ -12,13 +14,27 @@ const Counter = () => {
     setCount( count - 1 )
   }
 
+  const clickedButton = () => {
+    setClicked( clicked + 1 )
+  }
+
   return ( 
     <>
-      <h2>useState Hook</h2>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
-
+      <section className="mt-5 mb-2 pt-5 pb-4 bg-light border">
+        <h3 className="text-primary">Using the State Hook</h3>
+        <div className="d-flex justify-content-center align-items-center mt-4">
+          <button className="btn btn-secondary me-2" style={{width: '100px'}} onClick={decrement}>-</button>
+          <span className="fs-5 mx-5">{count}</span>
+          <button className='btn btn-primary ms-2' style={{width: '100px'}} onClick={increment}>+</button>
+        </div>
+      </section>
+      <section className="mt-5 mb-2 pt-5 pb-4 bg-light border">
+        <h3 className="text-primary">Using the State Hook 2</h3>
+        <div className="d-flex justify-content-center align-items-center mt-4">
+          <p className="mt-2">You has clicked the button<span className="text-danger fs-5 mx-2">{clicked}</span>times</p>
+          <button className='btn btn-danger ms-4' onClick={clickedButton}>click me</button>
+        </div>
+      </section>
     </>
   );
 }
