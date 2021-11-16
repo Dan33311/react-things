6c9aa4c27b29f65736e066d55a48cee7b88ae38d
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-11-15 21:14:36 -0500

    just add styles

--- src/components/ArrayY.js
+++ src/components/ArrayY.js
@@ -2,8 +2,8 @@ import { useState } from "react"
 
 const carsArr = () => [
   { id: 1, brand: 'Tesla', model: 'model x' },
-  { id: 2, brand: 'Renault', model: 'Duster' },
   { id: 3, brand: 'Ford', model: 'Endeavour' },
+  { id: 2, brand: 'Renault', model: 'Duster' },
 ]
 
 const ArrayY = () => {
@@ -29,23 +29,25 @@ const ArrayY = () => {
 
   return (  
     <section className="mt-5 mb-2 pt-5 pb-4 bg-light rounded-2">
-      <h3 className="text-primary">Arrayy</h3>
-      <div className="d-flex justify-content-center align-items-center mt-4">
-        <button className="btn btn-success" onClick={() => AddCar({ brand: "new brand", model: "new model" })}>Add</button>
+      <h3 className="text-primary">Array</h3>
+      <button type="button" className="btn btn-success mt-3 me-3" onClick={() => AddCar({ brand: "new brand", model: "new model" })}>Add</button>
+      <div className="d-flex justify-content-center mt-4">
 
         <div>
           {cars.map(car => (
             <div className="d-flex align-items-center" key={car.id} >
-              <h5 className="mt-2 me-2 ">Car:</h5>
-              <p className="mt-3 me-4">{car.brand}</p>
-              <button className="btn btn-danger" onClick={() => deleteCar(car.id)}>Remove</button>
-              <button className="btn btn-info" onClick={() => EditCar({ id: car.id, brand: "edited brand", model: "edit model" })}>Edit</button>
+              <h6 className="mt-2 me-2 ">Car:</h6>
+              <p className="mt-3 me-3 col-4 text-start">{car.brand}</p>
+              <button className="btn btn-danger me-2" onClick={() => deleteCar(car.id)}>Remove</button>
+              <button className="btn btn-info" onClick={() => EditCar({ id: car.id, brand: "edited brand", model: "edited model" })}>Edit</button>
             </div>
           ))}
         </div>
-        {/* <button className="btn btn-danger" onClick={() => changeState()}>Trigger an error</button> */}
-        {/* <p className="mx-4 mt-3"><span className="text-danger fs-5 mx-2">{error}</span></p> */}
-        {/* <button className="btn btn-info" onClick={() => setError("")}>Clear</button> */}
+
+        {/* printing the cars array */}
+        <div className="text-start mx-5">
+          <pre>{JSON.stringify(cars, null, 2)}</pre>
+        </div>
       </div>
     </section>
   );
